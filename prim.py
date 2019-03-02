c4d4511eaff0c3edd6ad01db6cc077d9ee180039
Paul Hubbard <phubbard67@gmail.com>
2019-03-01 21:10:37 -0800

    first commit

--- prim.py
+++ prim.py
@@ -86,17 +86,17 @@ f.close()
 #sudo code taken from https://en.wikipedia.org/wiki/Prim%27s_algorithm
 #
 #
-#    Associate with each vertex v of the graph a number
-#    C[v] (the cheapest cost of a connection to v)
-#    and an edge E[v] (the edge providing that cheapest connection).
+#    Todo: Associate with each vertex v of the graph a number
+#     C[v] (the cheapest cost of a connection to v)
+#     and an edge E[v] (the edge providing that cheapest connection).
 #
-#    To initialize these values, set all values of C[v] to +∞ (or to any number larger than the maximum edge weight)
-#    and set each E[v] to a special flag value indicating that there is no edge connecting v to earlier vertices.
+#    Todo:To initialize these values, set all values of C[v] to +∞ (or to any number larger than the maximum edge weight)
+#     and set each E[v] to a special flag value indicating that there is no edge connecting v to earlier vertices.
 #
-#    Initialize an empty forest F and a set Q of vertices that have not yet been included in
-#    F (initially, all vertices).
+#    Todo:Initialize an empty forest F and a set Q of vertices that have not yet been included in
+#     F (initially, all vertices).
 #
-#    Repeat the following steps until Q is empty:
+#    Todo:Repeat the following steps until Q is empty:
 #        Find and remove a vertex v from Q having the minimum possible value of C[v]
 #        Add v to F and, if E[v] is not the special flag value, also add E[v] to F
 #        Loop over the edges vw connecting v to other vertices w.
@@ -104,7 +104,7 @@ f.close()
 #        if w still belongs to Q and vw has smaller weight than C[w], perform the following steps:
 #            Set C[w] to the cost of edge vw
 #            Set E[w] to point to edge vw.
-#    Return F
+#     Return F
 #
 #
 #
